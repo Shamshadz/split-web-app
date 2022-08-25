61b65aeee47ae744acd45e7b1d1e78d6dc86c315
Shamshadz <chaudhary.s.shamshad07@gmail.com>
2022-08-25 13:47:01 +0530

    some changes

--- split/settings.py
+++ split/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-52$%bp%yg(=6romcpmz980zzb%lx2=_^rfrz4rl1qf-1w9942+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','splitbillapp.herokuapp.com']
 
 
 # Application definition
